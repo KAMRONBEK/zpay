6f1dfb41d41de1e8838431e3baa4301ec2aa92da
KAMRONBEK <kamuranbek1998@gmail.com>
2022-05-28 16:43:32 +0500

    minur version fixes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import 'react-native-gesture-handler';
 import CustomDrawer from './src/view/constants/CustomDrawer';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const App = () => {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <AppNavigator />
       <StatusBar translucent backgroundColor={'transparent'} />
     </GestureHandlerRootView>
